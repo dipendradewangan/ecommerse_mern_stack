a505f31c946801741755004466c6674980b179fd
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-08-13 23:04:35 +0530

    using mui i created product page and cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./component/Footer/Footer";
 import Header from "./component/Header/Header";
+import Mui from "./component/Mui/Mui";
 import Section from "./component/Section/Section";
 
 function App() {
   return (
     <div className="App">
 
-      
+      <Mui/>
       <Header/>
       <Section/>
       <Footer/>
--- frontend/src/component/Product/Cart/CartItem.js
+++ frontend/src/component/Product/Cart/CartItem.js
@@ -1,26 +1,35 @@
 import React from 'react'
+import { Autocomplete, TextField, Button } from '@mui/material'
+
+
+const quantityOption = [
+    { label: 1, value: 1 }
+]
 
 const CartItem = () => {
     return (
         <>
-            <div className='cart-item my-5'>
-                <div className='image-box'>
+            <div className='cart-item my-10'>
+                <div className='image-box mr-9'>
                     <img alt='product_mage' src='https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600' />
                 </div>
                 <div className='description-box px-5'>
                     <h1 className='cart-item-name font-bold text-2xl mb-5'>Lenovo G5080 8gb/1tb 256gb ssd core i3 4th generation</h1>
                     <h1 className='cart-item-price font-semibold text-xl'>$500</h1>
-                    <p className='cart-item-left font-bold text-lg'>4 item left</p>
-                    <div>
-                        <select className='item-quantity'>
-                            <option value="0">Quantity</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="0">3</option>
-                            <option value="0">4</option>
-                            <option value="0">5</option>
-                        </select>
-                        <button>Remove</button>
+                    <p className='cart-item-left font-semibold mb-2 text-lg'>4 item left</p>
+                    <div className='flex'>
+                        <Autocomplete 
+                            className='mr-2'
+                            disablePortal
+                            id="quentity-selector"
+                            options={quantityOption}
+                            style={{ width: "150px" }}
+                            size="small"
+                            renderInput={(params) => <TextField {...params} label="Quantity" />}
+
+                        />
+                        <Button size='small' variant="contained" color='secondary'>Remove</Button>
+
                     </div>
                 </div>
             </div>
