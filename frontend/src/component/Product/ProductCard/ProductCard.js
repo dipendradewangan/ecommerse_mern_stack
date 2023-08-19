7ae867c832f195eaa0eabf2e796cc451924e91aa
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-08-19 22:20:48 +0530

    added product data into webpage

--- frontend/src/component/Product/ProductCard/ProductCard.js
+++ frontend/src/component/Product/ProductCard/ProductCard.js
@@ -2,26 +2,28 @@ import React from 'react'
 import "./ProductCard.css"
 import ReactStars from 'react-rating-stars-component'
 
-const ratingOptions = {
-    count: 5,
-    size: 24,
-    activeColor: 'red',
-    value: 3.6,
-    onchange: false,
-    isHalf: true,
-    edit: false
-
-}
-
-const ProductCard = () => {
+const ProductCard = ({product, index}) => {
+    
+    const ratingOptions = {
+        count: 5,
+        size: 24,
+        activeColor: 'red',
+        value: product.rating,
+        onchange: false,
+        isHalf: true,
+        edit: false
+    
+    }
+    
+    console.log(index)
     return (
         <>
-            <div className='productCard'>
+            <div key={product.brandName} className='productCard'>
                 <img src='https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600' alt='productName' className='w-full' />
                 <div className='p-2'>
-                    <p className='text-lg font-semibold mb-2' style={{color : 'gray'}}>category</p>
-                    <h1 className='text-xl font-semibold mb-5' style={{color : '#555353'}}>Lenovo G5080 8gb/1tb 256gb ssd core i3 4th generation</h1>
-                    <h1 className='font-bold text-2xl' style={{color : "gray"}}>$150</h1>
+                    <p className='text-lg font-semibold mb-2' style={{color : 'gray'}}>{product.brandName} {product.category}</p>
+                    <h1 className='text-xl font-semibold mb-5' style={{color : '#555353'}}>{product.name}</h1>
+                    <h1 className='font-bold text-2xl' style={{color : "gray"}}>${product.price}</h1>
                     <ReactStars
                         {...ratingOptions}
                     />
--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -6,33 +6,25 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../../redux/actions/productActions'
 const Products = () => {
     const dispatch = useDispatch()
-    const products = useSelector(state=>state.products)
-    useEffect(()=>{
+    const products = useSelector(state => state.products.products)
+    useEffect(() => {
         dispatch(getProducts())
-    },[dispatch])
+    }, [dispatch])
 
-    console.log(products.products.length)
+    
 
     return (
         <>
+
             <button style={{ background: "red" }} onClick={() => dispatch(getProducts())}>Load</button>
             <div className='productContainer w-full p-10'>
                 <h1 className='font-semibold text-5xl text-center my-14'>Featured Products</h1>
                 <div className='productRenderBox'>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-
+                    {
+                        products.map((product,index) => {
+                            return <ProductCard product={product} index={index} />
+                        })
+                    }
                 </div>
             </div>
         </>
