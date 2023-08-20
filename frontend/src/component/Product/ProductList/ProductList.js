27ac5166af64dc0a3df04beebbe76e0acd2d2067
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-08-20 20:06:43 +0530

    added product listing page

--- frontend/src/component/Product/ProductList/ProductList.js
+++ frontend/src/component/Product/ProductList/ProductList.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../../../redux/actions/productActions'
 import "./ProductList.css"
-import IconButton from '@mui/material/IconButton'
-import DeleteIcon from '@mui/icons-material/Delete'
+import Button from '@mui/material/Button'
 
 
 const ProductList = () => {
@@ -20,45 +19,53 @@ const ProductList = () => {
     return (
         <>
             <div className='productListContainer'>
-                <h1>this is the product list</h1>
-                <table>
-                    <thead>
-                        <td>Product Id</td>
-                        <td>Product Name</td>
-                        <td>Brand</td>
-                        <td>Price</td>
-                        <td>Ratings</td>
-                        <td>Stock</td>
-                        <td>
-                            tools
-                        </td>
+                <div className='p-3 mb-10 flex justify-between align-middle border-t-2 border-b-2'>
+                    <h1 className='font-semibold text-4xl'>Products</h1>
+                    <Button variant="contained">New Product</Button>
+                </div>
+                <div>
+                    <table className='w-full'>
+                        <thead className='tableHeading'>
+                            <td className='productCol id'>
+                                <input type='checkbox' />
+
+                            </td>
+                            <td>Product Id</td>
+                            <td>Product Name</td>
+                            <td>Brand</td>
+                            <td>Price</td>
+                            <td>Ratings</td>
+                            <td>Stock</td>
+                            <td>tools</td>
 
 
-                    </thead>
-                    {
-                        products.map((product, index) => {
-                            return (
-                                <tr key={index} className='productRow'>
-                                    <td className='productCol id'>
-                                        <input type='checkbox' />
-                                    </td>
-                                    <td className='productCol id'>{product._id}</td>
-                                    <td className='productCol name'>
-                                        {product.name}
-                                        <p>{product.description}</p>
-                                    </td>
-                                    <td className='productCol'>{product.brandName}</td>
-                                    <td className='productCol'>₹{product.price}</td>
-                                    <td className='productCol'>{product.rating}</td>
-                                    <td className='productCol'>{product.stock}</td>
-                                    
-                                </tr>
-                            )
-                        })
-                    }
+                        </thead>
+                        {
+                            products.map((product, index) => {
+                                return (
+                                    <tr key={index} className='productRow'>
+                                        <td className='productCol id'>
+                                            <input type='checkbox' />
+                                        </td>
+                                        <td className='productCol id'>{product._id}</td>
+                                        <td className='productCol name'>
+                                            {product.name}
+                                            <p>{product.description}</p>
+                                        </td>
+                                        <td className='productCol'>{product.brandName}</td>
+                                        <td className='productCol' style={{ textAlign: 'right' }}>₹{product.price}</td>
+                                        <td className='productCol' style={{ textAlign: 'right' }}>{product.rating}</td>
+                                        <td className='productCol' style={{ textAlign: 'right' }}>{product.stock}</td>
+                                        <td className='productCol'>tools</td>
 
-                </table>
+                                    </tr>
+                                )
+                            })
+                        }
 
+                    </table>
+
+                </div>
             </div>
         </>
     )
