74eb7dcd2e7eb00700e36a2bba170f80d004f042
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-08-26 11:36:22 +0530

    designed nav

--- frontend/src/component/Nav/Nav.js
+++ frontend/src/component/Nav/Nav.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Button, Avatar, SpeedDial, SpeedDialAction } from '@mui/material'
-import { deepOrange, purple } from '@mui/material/colors'
-import { SpeedDialIcon } from '@mui/material'
+import React, { useState } from 'react'
+import { Button } from '@mui/material'
+import SpeedDialComponent from './SpeedDialComponent';
+
+const logged = false
 
 
 const Nav = () => {
+
+    const [modelOpen, setModalOpen] = useState(false)
     return (
         <>
             <nav className='flex justify-between p-4 border-4 relative'>
+                <div className='logo'>
+                    <img alt='logo' src='../images/logo.png' />
+                </div>
                 <ul className='flex font-bold gap-5' id='menu'>
                     <li>
                         <a href='/'>
@@ -19,44 +25,17 @@ const Nav = () => {
 
                 <div className='flex gap-5'>
                     {
-                        true ? (
-                            <>
-                                {/* <Button variant='text' color='secondary' sx={{ width: 'auto', height: "auto", borderRadius: '50%', padding: 0 }}>
-                                    <Avatar color="secondary" sx={{ bgcolor: purple[200], width: 60, height: 60, fontSize: 20, padding: 0 }} alt="dipendra Sharp" src="https://opengaph.githubassets.com/f290141da99370fb020ae1894bed592867c05e4871087e18c32a37a1b2d4cf79/mui/material-ui/issues/3510" />
-                                </Button> */}
-                                <SpeedDial
-                                    direction='down'
-                                    ariaLabel="SpeedDial basic example"
-                                    sx={{ position: 'absolute', right :15}}
-                                    icon={<Avatar color="secondary" sx={{ bgcolor: purple[200], width: 60, height: 60, fontSize: 20, padding: 0 }} alt="dipendra Sharp" src="https://opengaph.githubassets.com/f290141da99370fb020ae1894bed592867c05e4871087e18c32a37a1b2d4cf79/mui/material-ui/issues/3510" />}
-                                >
-
-                                    <SpeedDialAction
-                                        key={"key"}
-                                    />
-                                    <SpeedDialAction
-                                        key={"key"}
-                                    />
-                                    <SpeedDialAction
-                                        key={"key"}
-                                    />
-                                    <SpeedDialAction
-                                        key={"key"}
-                                    />
-
-
-
-                                </SpeedDial>
-                            </>
-
+                        logged ? (
+                            <SpeedDialComponent />
                         ) : (
                             <>
-                                <Button color="secondary" size="large" variant="outlined" sx={{ width: 120, height: 60, fontSize: 20 }}>Login</Button>
-                                <Button color="secondary" size="large" variant="contained" sx={{ width: 120, height: 60, fontSize: 20 }}>Signup</Button>
+                                <Button onClick={()=>setModalOpen(!modelOpen)} color="secondary" size="large" variant="outlined" sx={{ width: 120, height: 60, fontSize: 20 }}>Login</Button>
+                                <Button color="secondary" size="large" variant="contained" sx={{ width: 120, height: 60, fontSize: 20, marginRight: 2 }}>Signup</Button>
                             </>
                         )
                     }
                 </div>
+
             </nav>
 
         </>
---
+++ frontend/src/component/Nav/SpeedDialComponent.js
@@ -0,0 +1,92 @@
+import { Box, Backdrop, SpeedDial, SpeedDialAction, Avatar } from '@mui/material';
+import { purple } from '@mui/material/colors';
+import React, { useState } from 'react'
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+
+
+const admin = true;
+
+
+// speed dial activity api
+const actions = [
+  {
+    icon: <ManageAccountsIcon />,
+    title: "Account"
+  },
+  {
+    icon: <ShoppingCartIcon />,
+    title: "Cart"
+  },
+  {
+    icon: <LocalShippingIcon />,
+    title: "Orders"
+  },
+  {
+    icon: <AccountCircleIcon />,
+    title: "Profile"
+  }
+]
+
+
+
+// exported component
+const SpeedDialComponent = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1 }}>
+        <Backdrop open={open} />
+        <SpeedDial
+          direction='down'
+          ariaLabel="SpeedDial tooltip example"
+          sx={{ position: 'absolute', top: 0, right: 16}}
+          icon={<Avatar color="secondary" sx={{ bgcolor: purple[100], width: 60, height: 60, fontSize: 20, padding: 0 }} alt="dipendra Sharp" src="https://opengaph.githubassets.com/f290141da99370fb020ae1894bed592867c05e4871087e18c32a37a1b2d4cf79/mui/material-ui/issues/3510" />}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          open={open}
+        >
+          {
+            actions.map((action) =>
+              <SpeedDialAction
+                
+                key={"key"}
+                icon={action.icon}
+                tooltipTitle={action.title}
+                tooltipOpen
+                onClick={handleClose}
+              />
+
+            )
+
+          }
+
+          {
+            admin 
+            ? (
+              <SpeedDialAction
+                
+                key={"key"}
+                icon={<DashboardIcon/>}
+                tooltipTitle={"Dashboard"}
+                tooltipOpen
+                onClick={handleClose}
+              />
+            ):''
+          }
+
+
+
+        </SpeedDial>
+      </Box>
+    </>
+  )
+}
+
+export default SpeedDialComponent
